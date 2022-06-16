5ac6649074ea13e0a53c1389902b2e48e9bd6cab
Oleh Borodin <94630637+OlehBorodin@users.noreply.github.com>
2022-06-16 18:56:31 +0200

    Commit&push from IDEA

--- src/borodinOleh/hw2/ReadFile.java
+++ src/borodinOleh/hw2/ReadFile.java
@@ -1,2 +1,30 @@
-package borodinOleh.hw2;public class ReadFile {
+package borodinOleh.hw2;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+class ReadFile {
+    public static void readTxtFile(String fileName) {
+
+        try {
+
+            FileReader tfr = new FileReader(fileName);
+            char[] buffer = new char[8096];
+
+            int chars = tfr.read(buffer);
+
+            while (chars != -1) {
+                System.out.println(String.valueOf(buffer, 0, chars));
+                chars = tfr.read(buffer);
+            }
+
+            tfr.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
+
--- src/borodinOleh/hw2/Runner.java
+++ src/borodinOleh/hw2/Runner.java
@@ -1,8 +1,14 @@
-package homework.hw2;
+package borodinOleh.hw2;
+
+import java.io.Reader;
+
+import static borodinOleh.hw2.ReadFile.readTxtFile;
 
 public class Runner {
 
     public static void main(String[] args) {
-        TraningArray.sumArrayElements();
+
+       TraningArray.sumArrayElements();
+        readTxtFile("/Users/olegborodi/Desktop/text.txt");
     }
 }
