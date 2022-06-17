79bc333bb4d37362595acfbc28b15611bb398cf6
Anastasia Parfilo <parfilo2003@gmail.com>
2022-06-17 15:24:00 +0300

    delete

--- src/anastasia_parfilo/hw1/ArraysMain.java
+++
@@ -1,9 +0,0 @@
-package anastasia_parfilo.hw1;
-
-import java.io.IOException;
-
-public class ArraysMain {
-    public static void main(String[] args) throws IOException {
-        RunProgram.start();
-    }
-}
