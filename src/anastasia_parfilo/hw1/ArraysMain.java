422a5313c0cf54cbbcd0b2810d6f6765a4e56958
Anastasia Parfilo <parfilo2003@gmail.com>
2022-06-17 16:02:04 +0300

    hw1

---
+++ src/anastasia_parfilo/hw1/ArraysMain.java
@@ -0,0 +1,9 @@
+package anastasia_parfilo.hw1;
+
+import java.io.IOException;
+
+public class ArraysMain {
+    public static void main(String[] args) throws IOException {
+        RunProgram.start();
+    }
+}
